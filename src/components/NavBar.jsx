f13f99c7176981a66b1f8bc490499559f19b48df
Aldi <m.naufaldi.aldi@gmail.com>
2023-02-14 15:14:16 +0700

    updated navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,19 +3,29 @@ import SearchBar from './SearchBar'
 
 const NavBar = () => {
   return (
-    <div className='flex items-center justify-evenly h-[80px]'>
+    <div className='flex items-center justify-evenly h-[80px] shadow-md'>
         <div>
-            <h1 className='font-poppins font-[700] text-[24px] mx-10'>hitorigoto</h1>
+            <h1 className='font-poppins font-[700] text-[24px] ml-[10px]'>hitorigoto</h1>
         </div>
         <SearchBar />
-        <ul className='flex justify-between'>
-            <li>
-                <a href="/#" className='font-[400] text-[16px] mx-[48px]'>JLPT</a>
-            </li>
-            <li>
-                <a href="/#" className='font-[400] text-[16px] mx-[48px]'>Courses</a>
-            </li>
-        </ul>
+        <div>
+            <ul className='flex justify-between gap-[48px]'>
+                <li>
+                    <a href="/#" className='font-[400] text-[16px] px-[25px]'>JLPT</a>
+                </li>
+                <li>
+                    <a href="/#" className='font-[400] text-[16px] px-[25px]'>Courses</a>
+                </li>
+            </ul>
+        </div>
+        <div className='flex justify-between gap-[28px]'>
+            <button className='bg-transparent border-2 border-[#645CBB] px-2 py-1 rounded-md '>
+                Login 
+            </button>
+            <button>
+                Sign up
+            </button>
+        </div>
     </div>
   )
 }
