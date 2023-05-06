b05ac7bb5b06ed67e41acaf07af4eb815e46c750
Irsyad <azcat01@yahoo.co.id>
2023-05-06 12:56:47 +0700

    add error validator on createAccount

--- server/api/controller.js
+++ server/api/controller.js
@@ -1,22 +1,105 @@
-const path = require('path');
-const express = require('express');
-const app = express();
+const bcrypt = require('bcrypt');
+const users = require('./user');
+const { validationResult } = require('express-validator');
 
-const rootPath = '../../../'
+// const homepage = async (req, res, next) => {
+//   try {
+//     res.render()
+//     return res.status(200).json({
+//       status: "OK",
+//       message: "Success"
+//     });
+//   } catch (error) {
+//     return res.status(500).json({
+//       status: "FAILED",
+//       message: error
+//     });
+//   }
+// };
 
-const getHomepage = async (req, res, next) => {
+const createAccount = async (req, res, next) => {
   try {
-    res.sendFile(path.join(__dirname + rootPath + '/build/index.html'));
+    const { username, email } = req.body;
+    const encryptedPassword = await bcrypt.hash(req.body.password, 10);
+    const errorResult = validationResult(req).formatWith(error => {
+      if (error.path === "password") {
+        error.value = encryptedPassword
+      }
+    });
+    errorResult.array();
+    
+    if (errorResult.isEmpty()) {
+      const userAccount = {
+        username,
+        email,
+        encryptedPassword
+      };
+
+      users.push(userAccount);
+
+      return res.status(201).json({
+        status: "Created",
+        message: "Account successfully created",
+        data: userAccount
+      });
+    } else {
+      return res.status(400).json({
+        status: "Bad Request",
+        message: errorResult,
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      status: "Internal Server Error",
+      message: error,
+    });
+  }
+};
+
+const getAccount = async (req, res, next) => {
+  try {
+    const userAccount = users.find((user) => req.params.username === user.username);
+    
+    if (userAccount !== undefined) {
+      return res.status(200).json({
+        status: "OK",
+        message: "Success",
+        data: userAccount
+      });
+    } else {
+      return res.status(404).json({
+        status: "Not found",
+        message: "Account not found",
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      status: "Internal Server Error",
+      message: error,
+    });
+  }
+};
+
+const deleteAccount = async (req, res, next) => {
+  try {
+
+    users.pop(req.params.username);
+    
     return res.status(200).json({
-      status: "OK"
+      status: "OK",
+      message: "Account has been deleted",
+      data: users,
     });
   } catch (error) {
     return res.status(500).json({
-      status: "FAILED"
+      status: "Internal Server Error",
+      message: error,
     });
   }
 };
 
 module.exports = {
-  getHomepage
+  createAccount,
+  getAccount,
+  deleteAccount
 }
