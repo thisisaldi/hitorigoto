30116b1c55e8cb1e5eba1347bb2d66acc5ba041f
espadav <davidsiddii@gmail.com>
2023-06-03 20:22:41 +0700

    added new route editprofile

--- src/components/AnimatedRoutes.jsx
+++ src/components/AnimatedRoutes.jsx
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
+import EditProfile from '../pages/EditProfile';
 
 const AnimatedRoutes = () => {
 
     const location = useLocation();
-
+    const [user,setUser] = useState("");
+    // console.log(user)
     return (
         <AnimatePresence initial={false}>
             <Routes location={location} key={location.pathname}>
-                <Route path='/' element={<Home />} />
+                <Route path='/' element={<Home user={user}/>} />
                 <Route path='/login' element={<Login />} />
                 <Route path='/register' element={<Register />} />
+                <Route path='/editprofile' element={<EditProfile user ={user} setUser={setUser}/>} />
+
             </Routes>
         </AnimatePresence>
     )
