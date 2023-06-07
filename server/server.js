c241e6d57501d76736d240d879d1859d4f2265ab
Irsyad <azcat01@yahoo.co.id>
2023-06-07 11:23:21 +0700

    setup dbconnect

--- server/server.js
+++ server/server.js
@@ -2,24 +2,17 @@ require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-const path = require('path');
 const { env, port } = require("./api/configs/server.config");
-// const dbConnect = require('./api/configs/db.config');
-const { notFound, errorHandler } = require('./api/middleware/error');
 const router = require("./api/router");
+const dbConnect = require('./api/configs/db.config');
 
 const app = express();
 
+dbConnect;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
-
-app.use("/api/users", router);
-app.get('/', (req, res) => {
-  res.send('API is running....');
-});
-app.use(notFound);
-app.use(errorHandler);
+app.use("/", router);
 
 app.listen(port,() => console.log(`Server started on [env, port] = [${env}, ${port}]`));
