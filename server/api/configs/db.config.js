33dadc1655f3b35a738a6603cb75c4b9c9c4d5bd
Irsyad <azcat01@yahoo.co.id>
2023-06-07 11:25:09 +0700

    setup dbconnect configs

--- server/api/configs/db.config.js
+++ server/api/configs/db.config.js
@@ -1,7 +1,13 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
 
-const dbConnect = async () => {
-  
-};
+const uri = process.env.ATLAS_URI
+
+const dbConnect = mongoose.connect(
+  uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true})
+    .then(() => console.log("MongoDB is  connected successfully"))
+    .catch((err) => console.error(err));
 
 module.exports = dbConnect;
