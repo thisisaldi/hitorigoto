7a97f9565c4f35f3757e12727b758df0732400e9
Irsyad <azcat01@yahoo.co.id>
2023-06-12 11:39:15 +0700

    add schema for image database

--- server/api/model.js
+++ server/api/model.js
@@ -1,3 +1,4 @@
+const { ObjectId } = require('mongodb');
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
@@ -10,7 +11,34 @@ const userSchema = new mongoose.Schema({
   createdAt: {
     type: Date,
     default: new Date(),
-  },
+  }
+}, {
+  collection: "users",
+  versionKey: false,
 });
 
-module.exports = mongoose.model("User", userSchema)
+const imgSchema = new mongoose.Schema({
+    _id: ObjectId,
+    files_id: ObjectId,
+    n: Number,
+    data: Buffer,
+});
+
+const imgDataSchema = new mongoose.Schema({
+  _id: ObjectId,
+  length: Number,
+  chunkSize: Number,
+  uploadDate: Date,
+  filename: String,
+  contentType : String
+});
+
+const User = mongoose.model("User", userSchema);
+const Image = mongoose.model("Image", imgSchema, "fs.chunks");
+const ImageData = mongoose.model("ImageData", imgDataSchema, "fs.files");
+
+module.exports = {
+  User,
+  Image,
+  ImageData
+}
