80b44ee1ba586310ba0279a9518f1b05dbafa15e
Irsyad <azcat01@yahoo.co.id>
2023-06-12 11:40:00 +0700

    add edit profile endpoint

--- server/api/router.js
+++ server/api/router.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const controller = require("./controller")
+const upload = require('./util/upload.js')
 const router = express.Router();
 const { createAccountValidator, loginValidator } = require('./util/validator');
 
 router.get("/", controller.homePage);
-router.get("*", controller.invalidPage)
+router.get("/edit/:username", controller.getAccount);
+router.get("*", controller.invalidPage);
 
-router.post("/login", loginValidator, controller.getAccount);
+router.post("/login", loginValidator, controller.loginAccount);
 router.post("/register", createAccountValidator, controller.createAccount);
+router.post("/edit/:username", upload, controller.editAccount);
 
 router.delete('/edit/:username', controller.deleteAccount);
 
